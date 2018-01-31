4d7e2471033a300336acc9c39d5db8ab1ca6402e
apershic <apershic@sfu.ca>
2018-01-31 14:19:57 -0800

    trying to write some js

--- Assignment 1/ass1.js
+++ Assignment 1/ass1.js
@@ -1,4 +1,12 @@
-function greyify()
+
+boxes = document.getElementsByTagName('td');
+
+for (var i = 0; i < boxes.length; i++)
 {
-    this.class = "grey"
+    console.log('hi');
+    boxes[i].addEventListener("click", function()
+    {
+        this.style.backgroundColor = "grey";
+    });
 }
+
