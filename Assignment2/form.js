5661c5bf30b6cde816128ebc3b35e0ae44ae2903
ajpershick <ajpershick@gmail.com>
2018-02-17 14:43:23 -0800

    stuck on how to implement reads

---
+++ Assignment2/form.js
@@ -0,0 +1,15 @@
+var form = document.getElementById('form');
+var email = document.getElementById('email');
+var addEmailButton = document.getElementById('addEmailButton');
+var addUserButton = document.getElementById('addUserButton');
+var submit = document.getElementById('submit');
+
+// function addEmail() {
+//     br = document.createElement('br');
+//     email.appendChild(br);
+//     newEmail = email;
+//     email.appendChild(newEmail);
+//     console.log('created new email');
+// }
+//
+// addEmailButton.addEventListener('click', addEmail());
--- Assignment2/server.js
+++ Assignment2/server.js
@@ -1,11 +1,35 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var url = require('url');
+var qs = require('querystring');
 
 var server = http.createServer();
 server.on('request', function(req,res) {
+
     console.log('request:', req.url);
 
+//     if (req.method === 'GET' && req.url === '/') {
+//         res.writeHead(200, {"Content-Type": "text/html"});
+//         //var formStream = fs.createReadStream('./form.html');
+//         //console.log('created stream');
+//         //formStream.on('open', function() {
+//         //    formStream.pipe(res);
+//         //});
+//         var form = fs.readFileSync('./form.html');
+//         res.write(form);
+//     }
+//     if (req.method === 'GET' && req.url === '')
+//     else {
+//         res.writeHead(404);
+//         var error = fs.readFileSync('./404.html');
+//         res.write(error);
+//         console.log('Page not found');
+//     }
+//     res.end();
+// });
+    var urlObj = url.parse(req.url, true); // true => query turned into an obj
+    console.log(urlObj.query.lname);
     if (req.method === 'GET' && req.url === '/') {
         res.writeHead(200, {"Content-Type": "text/html"});
         //var formStream = fs.createReadStream('./form.html');
@@ -16,13 +40,31 @@ server.on('request', function(req,res) {
         var form = fs.readFileSync('./form.html');
         res.write(form);
     }
-    else {
-        res.writeHead(404);
-        var error = fs.readFileSync('./404.html');
-        res.write(error);
-        console.log('Page not found');
-    }
-    res.end();
+    else if (req.method === 'GET' && req.url.match(/^\/.+\.html$/)) {
+        var filepath = path.join(__dirname, req.url);
+        fs.readFile(filepath, function (err, contents) {
+            if (err) {
+                throw err;
+            } else {
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.write(contents);
+                res.end();
+            }
+        });
+
+    } //else if (req.method === 'GET' && req.url.match(/^\/.+\.js$/)) {
+    //     var jspath = path.join(__dirname, req.url);
+    //
+    //     fs.readFile(jspath, function (err, contents) {
+    //         if (err) {
+    //             throw err;
+    //         } else {
+    //             res.writeHead(200, {"Content-Type": "text/javascript"});
+    //             res.write(contents);
+    //             res.end();
+    //         }
+    //     });
+    // }
 });
 
 server.listen(8080);
