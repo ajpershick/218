df2eb975add4f18314675e8402b5f962e7899de6
ajpershick <ajpershick@gmail.com>
2018-02-17 12:13:27 -0800

    reverted back to synchronous reading

--- Assignment2/server.js
+++ Assignment2/server.js
@@ -3,29 +3,24 @@ var fs = require('fs');
 var path = require('path');
 
 var server = http.createServer();
-server.on('request', function(req,res){
+server.on('request', function(req,res) {
     console.log('request:', req.url);
-    if (req.url.match(/^\/.+\.html$/)){
-        var filepath = path.join(__dirname,req.url);
 
-        fs.readFile(filepath, function(err, contents){
-            if(err){
-                // handle error
-            } else {
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.write(contents);
-            }
-        });
-    }
-    else if (req.url === '/image.jpg'){
-        res.writeHead(200, {"Content-Type": "image/jpeg"});
-        var contents = fs.readFileSync('./image.jpg');
-        res.write(contents);
+    if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        //var formStream = fs.createReadStream('./form.html');
+        //console.log('created stream');
+        //formStream.on('open', function() {
+        //    formStream.pipe(res);
+        //});
+        var form = fs.readFileSync('./form.html');
+        res.write(form);
     }
     else {
         res.writeHead(404);
-        res.write('404 Error');
-        console.log('error goes here.');
+        var error = fs.readFileSync('./404.html');
+        res.write(error);
+        console.log('Page not found');
     }
     res.end();
 });
