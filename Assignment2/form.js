4062f56e5530b99c160769eefd6c279a453d9388
ajpershick <ajpershick@gmail.com>
2018-02-19 01:12:45 -0800

    finish ass2

--- Assignment2/form.js
+++ Assignment2/form.js
@@ -1,15 +1,49 @@
-var form = document.getElementById('form');
-var email = document.getElementById('email');
+var form = document.getElementById('form1');
+var email = document.getElementById('email1').cloneNode(true);
 var addEmailButton = document.getElementById('addEmailButton');
 var addUserButton = document.getElementById('addUserButton');
 var submit = document.getElementById('submit');
+var numUsers = 1;
+var forms = document.getElementById('forms');
+var count = 2;
+//var emailDiv = document.getElementById('emails1');
+var blankForm = form.cloneNode(true);
 
-// function addEmail() {
-//     br = document.createElement('br');
-//     email.appendChild(br);
-//     newEmail = email;
-//     email.appendChild(newEmail);
-//     console.log('created new email');
-// }
-//
-// addEmailButton.addEventListener('click', addEmail());
+function addEmail() {
+
+    if (count > 0) {
+        var userEmail = this.parentElement.email;
+        var emailDiv = this.parentElement.getElementsByTagName('div')[0];
+        var newEmail = email.cloneNode(true);
+        emailDiv.appendChild(newEmail);
+        console.log('created new email');
+        count -=1;
+    }
+    else {
+        alert('3 emails is enough...')
+    }
+}
+
+function addUser() {
+    numUsers +=1;
+    var br = document.createElement('br');
+    var newUser = blankForm.cloneNode(true);
+    newUser.id = 'form' + numUsers;
+    newUser.getElementsByTagName('div')[0].id = 'emails' + numUsers;
+    newUser.getElementsByTagName('input')[2].id = 'email' + numUsers;
+    var newUserButton = newUser.getElementsByTagName('button')[0];
+    newUser.getElementsByTagName('button')[0].id = 'button' + numUsers;
+    newUserButton.addEventListener('click', addEmail);
+    forms.appendChild(newUser);
+    count += 2;
+
+}
+
+addEmailButton.addEventListener('click', addEmail);
+addUserButton.addEventListener('click', addUser);
+
+// submit.addEventListener('click', function() {
+//     alert('Thanks, your information is saved!');
+// });
+
+
--- Assignment2/server.js
+++ Assignment2/server.js
@@ -3,69 +3,91 @@ var fs = require('fs');
 var path = require('path');
 var url = require('url');
 var qs = require('querystring');
+var arr = [];
 
 var server = http.createServer();
 server.on('request', function(req,res) {
 
     console.log('request:', req.url);
+    if (req.method === 'GET') {
 
-//     if (req.method === 'GET' && req.url === '/') {
-//         res.writeHead(200, {"Content-Type": "text/html"});
-//         //var formStream = fs.createReadStream('./form.html');
-//         //console.log('created stream');
-//         //formStream.on('open', function() {
-//         //    formStream.pipe(res);
-//         //});
-//         var form = fs.readFileSync('./form.html');
-//         res.write(form);
-//     }
-//     if (req.method === 'GET' && req.url === '')
-//     else {
-//         res.writeHead(404);
-//         var error = fs.readFileSync('./404.html');
-//         res.write(error);
-//         console.log('Page not found');
-//     }
-//     res.end();
-// });
-    var urlObj = url.parse(req.url, true); // true => query turned into an obj
-    console.log(urlObj.query.lname);
-    if (req.method === 'GET' && req.url === '/') {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        //var formStream = fs.createReadStream('./form.html');
-        //console.log('created stream');
-        //formStream.on('open', function() {
-        //    formStream.pipe(res);
-        //});
-        var form = fs.readFileSync('./form.html');
-        res.write(form);
+        if (req.url === '/') {
+            var formPath = path.join(__dirname,'/form.html');
+            var formStream = fs.createReadStream(formPath, { highWaterMark: 1024 });
+            res.writeHead(200, {"Content-Type": "text/html"});
+            formStream.pipe(res);
+        }
+        else if (req.url === '/form.css') {
+            var cssPath = path.join(__dirname,req.url);
+            var cssStream = fs.createReadStream(cssPath, { highWaterMark: 1024 });
+            res.writeHead(200, {"Content-Type": "text/css"});
+            cssStream.pipe(res);
+        }
+        else if (req.url === '/form.js') {
+            var jsPath = path.join(__dirname,req.url);
+            var jsStream = fs.createReadStream(jsPath, { highWaterMark: 1024 });
+            res.writeHead(200, {"Content-Type": "application/javascript"});
+            jsStream.pipe(res);
+        }
+        else if (req.url === '/data/users.json') {
+            var usersPath = path.join(__dirname,'/data/users.json');
+            fs.exists(usersPath, function(exists) {
+                if (!exists) {
+                    jsonobject = JSON.stringify(arr);
+                    fs.writeFile(usersPath, jsonobject, function (err) {
+                        if (err) throw err;
+                    });
+                }
+            });
+
+            var jsonPath = path.join(__dirname,req.url);
+            var jsonStream = fs.createReadStream(jsonPath, { highWaterMark: 1024 });
+            res.writeHead(200, {"Content-Type": "application/json"});
+            jsonStream.pipe(res);
+        }
+
+        else if (req.url === '/users.html') {
+            var userPath = path.join(__dirname,req.url);
+            var userStream = fs.createReadStream(userPath, { highWaterMark: 1024 });
+            res.writeHead(200, {"Content-Type": "text/html"});
+            userStream.pipe(res);
+        }
+
+        else {
+            res.writeHead(404);
+            res.write('<h1>404 Error: Page Not Found</h1>');
+            res.end();
+        }
     }
-    else if (req.method === 'GET' && req.url.match(/^\/.+\.html$/)) {
-        var filepath = path.join(__dirname, req.url);
-        fs.readFile(filepath, function (err, contents) {
-            if (err) {
-                throw err;
-            } else {
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.write(contents);
-                res.end();
-            }
-        });
 
-    } //else if (req.method === 'GET' && req.url.match(/^\/.+\.js$/)) {
-    //     var jspath = path.join(__dirname, req.url);
-    //
-    //     fs.readFile(jspath, function (err, contents) {
-    //         if (err) {
-    //             throw err;
-    //         } else {
-    //             res.writeHead(200, {"Content-Type": "text/javascript"});
-    //             res.write(contents);
-    //             res.end();
-    //         }
-    //     });
-    // }
+    else if (req.method === 'POST' && req.url === '/'){
+        var usersPath = path.join(__dirname,'/data/users.json');
+        var body ='';
+        req.on('data', function(data){
+            body += data.toString();
+            res.redirect('/UserHomePage');
+            res.end();
+        });
+        req.on('end', function(){
+            var postObj = qs.parse(body);
+            console.log(postObj);
+            console.log(arr);
+            arr.push(postObj);
+            jsonarray = JSON.stringify(arr);
+            fs.writeFile(usersPath, jsonarray, function (err) {
+                if (err) throw err;
+                res.end();
+            });
+        });
+        res.end();
+    }
+    else {
+        res.writeHead(404);
+        res.write('<h1>404 Error: Page Not Found</h1>');
+        res.end();
+    }
+
 });
 
-server.listen(8080);
-console.log('Magic is happening on port 8080');
+server.listen(23734);
+console.log('Magic is happening on port 23734');
