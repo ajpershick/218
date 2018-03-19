c493cd36f1caa6eb0e231dd87a44e49855e372ef
Alec <ajpershick@gmail.com>
2018-03-19 11:20:46 -0700

    set up vue

--- Ass3/server.js
+++ Ass3/server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', function (req, res) {
-    res.send('Home');
-});
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm','html'],
+    index: "index.html"
+};
+
+app.use('/', express.static('./pub_html', options));
 
 app.listen(port, function () {
     console.log('Server listening on port 5000!');
