30fb5835f83ff0cbe996caf168e8dec30e6b7dff
Alec <ajpershick@gmail.com>
2018-03-23 13:50:33 -0700

    changed to single Vue

--- Ass3/pub_html/login.js
+++ Ass3/pub_html/login.js
@@ -1,34 +1,23 @@
 const login = new Vue({
-  el: '#login',
+  el: '#app',
   data: {
     username: '',
     password: '',
-    showLogin: false,
-  }
-});
-
-const landing = new Vue({
-  el: '#landing',
-  data: {
-    showLanding: false,
-  }
-});
-
-const checking = new Vue({
-  el: '#landing',
-  data: {
     checkin: '',
-      showChecking: false,
-  }
-});
-
-const checkin = new Vue({
-  el: '#check-in',
-  data: {
     string: '',
     name: '',
     id: '',
-      showCheckin: false,
+    show: 'login',
   }
 });
+
+
+function logIn(){
+  axios.post('/login', {
+    username: this.username,
+    password: this.password,
+  });
+}
+
+
 
--- Ass3/server.js
+++ Ass3/server.js
@@ -3,19 +3,27 @@ const app = express();
 const port = 5000;
 
 const options = {
-    dotfiles: 'ignore',
-    etag: false,
-    extensions: ['htm','html'],
-    index: "index.html"
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm','html'],
+  index: "index.html"
 };
 
 app.use('/', function(req,res,next){
-    console.log(req.method, 'request:', req.url, JSON.stringify(req.body));
-    next();
+  console.log(req.method, 'request:', req.url, JSON.stringify(req.body));
+  next();
 });
 
 app.use('/', express.static('./pub_html', options));
 
+function logIn(){
+
+}
+
+
+
+
+
 app.listen(port, function () {
-    console.log('Server listening on port 5000!');
+  console.log('Server listening on port 5000!');
 });
