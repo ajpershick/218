d3d6d02d03c4e350a54f95578ce39b3340aefaf7
Alec <ajpershick@gmail.com>
2018-03-23 20:28:41 -0700

    started using mongoose

--- Ass3/pub_html/login.js
+++ Ass3/pub_html/login.js
@@ -1,28 +1,92 @@
+Vue.use(VeeValidate); // good to go.
 const app = new Vue({
-  el: '#app',
-  data: {
-    username: '',
-    password: '',
-    checkin: '',
-    string: '',
-    name: '',
-    id: '',
-    show: 'login',
-  }
+    el: '#app',
+    data: {
+        username: 'admin',
+        password: '1234',
+        checkIn: '',
+        string: '',
+        name: '',
+        id: '',
+        show: 'login',
+        activeCheckIn: false
+    },
+    methods: {
+        logIn: function(){
+            axios.post('/login', {
+                username: this.username,
+                password: this.password,
+            })
+                .then(function (response) {
+                    console.log('success');
+                    checkForm();
+                    app.show = 'landing'
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+        },
+
+        Checking: function(){
+            axios.post('/checking', {
+                checkIn_ID: this.checkIn
+            })
+                .then(function (response) {
+                    console.log('Check-in is active now');
+                    app.activeCheckin = true;
+                    app.show = 'checking';
+                    alert('Check-ins are now active')
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+        },
+
+        stopChecking: function(){
+            console.log('Check-in is active now');
+            app.activeCheckIn = false;
+            app.show = 'checked-in';
+            alert('Check-ins are now disabled');
+        },
+
+        goToLogIn: function(){
+            app.show = 'login';
+        },
+
+        CheckIn: function(){
+            axios.post('/checkIns', {
+                checkIn_ID: this.checkIn
+            })
+            .then(function (response) {
+                if (app.activeCheckIn){
+                    app.show = 'thanks';
+                    alert('Check-in confirmed!')
+                }
+                else{
+                    alert('Check-ins are not active right now. Try again later.')
+                }
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+        },
+
+        goToCheckIn: function(){
+            app.show = 'check-in';
+        },
+
+        goToHistory: function(){
+            app.show = 'history';
+        },
+    }
 });
 
-function logIn(){
-  axios.post('/login', {
-    username: app.username,
-    password: app.password,
-    })
-    .then(function (response) {
-      app.show = 'landing'
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
+function checkForm(){
+    this.errors = [];
+    if (!this.username) this.errors.push("username required.");
+    if (!this.password) this.errors.push("password required.");
+    if (this.errors !== '')
+    {
+        return 1;
+    }
 }
-
-
-
--- Ass3/server.js
+++ Ass3/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
+let adminUsername = 'admin';
+let adminPassword = '1234';
+let checkIns = [];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const options = {
   dotfiles: 'ignore',
@@ -9,21 +16,91 @@ const options = {
   index: "index.html"
 };
 
+// const MongoClient = require('mongodb').MongoClient;
+//
+// const url = "mongodb://localhost";
+//
+// MongoClient.connect(url, function(err, client){
+//     if (err) console.log(err);
+//     //console.log('connected');
+//     let database = client.db('Ass3DB'); // use
+//     let collection = database.collection('documents'); // db.documents
+//     collection.insertMany([{a:1,b:2},{a:3,b:4}], function(err, result){
+//         collection.find().forEach(function(mydoc){
+//             console.log(mydoc);
+//         });
+//     });
+// });
+
+
+/////// Connecting to Mongoose ////////
+///////////////////////////////////////
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/Ass3DB");
+let db = mongoose.connection;
+
+//db.on('error', function(){});
+db.once('open', function(){
+    console.log('connection success');
+});
+
+// Schema is a constructor
+let Schema = mongoose.Schema;
+// instantiate the constructor
+let adminSchema = new Schema({
+    username: { type:String },
+    password: { type:String, minlength:4 }
+});
+// create a new model
+let Admin = mongoose.model('admin', adminSchema);
+
+let admin1 = new Admin({username: 'admin', password: '1234'});
+// admin1.save();
+
+Admin.find(function (err, admins) {
+    if (err) return console.error(err);
+    // console.log(admins);
+});
+
+let sessionSchema = new Schema({
+    active: { type:Boolean },
+});
+// create a new model
+let Session = mongoose.model('session', sessionSchema);
+
+let session = new Session({active: false});
+// session.save();
+
+Session.find(function (err, sessions) {
+  if (err) return console.error(err);
+  console.log(sessions);
+});
+
 app.use('/', function(req,res,next){
-  console.log(req.method, 'request:', req.url, JSON.stringify(req.body));
+  console.log(req.method, 'request:', req.url, (req.body));
   next();
 });
 
 app.use('/', express.static('./pub_html', options));
 
-function logIn(){
+app.post('/login', function(req, res){
+  logInValidation();
+  res.end();
+});
 
-}
+app.post('/checking', function(req, res){
+  res.end();
+});
 
-
-
-
+app.post('/checkIns', function(req, res){
+  res.end();
+});
 
 app.listen(port, function () {
   console.log('Server listening on port 5000!');
 });
+
+function logInValidation(username, password){
+}
